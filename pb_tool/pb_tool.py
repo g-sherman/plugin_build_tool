b75b2e47d6dc6574fadb89d2e694d6629b87ee2a
Gary Sherman <gsherman@geoapt.com>
2014-09-24 16:31:05 -0800

    Semi-working version

--- pb_tool/pb_tool.py
+++ pb_tool/pb_tool.py
@@ -3,7 +3,10 @@ import os
 import sys
 import subprocess
 import zipfile
+import shutil
+import errno
 import ConfigParser
+
 
 import click
 
@@ -16,6 +19,19 @@ def echo():
     """Example script."""
     click.echo('Hello World!')
 
+
+def get_install_files():
+    cfg = config()
+    python_files = cfg.get('files', 'python_files').split()
+    main_dialog = cfg.get('files', 'main_dialog').split()
+    compiled_ui_files = cfg.get('files', 'compiled_ui_files').split()
+    extras = cfg.get('files', 'extras').split()
+    # merge the file lists
+    install_files = python_files + main_dialog + compiled_ui() + compiled_resource() + extras
+    #click.echo(install_files)
+    return install_files
+
+
 @cli.command()
 def deploy():
     """Deploy the plugin using parameters in pb_tool.cfg"""
@@ -23,31 +39,81 @@ def deploy():
     if not os.path.exists('pb_tool.cfg'):
         click.echo("Configuration file pb_tool.cfg is missing.")
     else:
+        print """Deploying will:
+        * Remove your currently deployed version
+        * Compile the ui and resource files
+        * Copy everything to the .qgis/python/plugins directory
+        """
+
+        plugin_dir = os.path.join(get_plugin_directory(), config().get('plugin', 'name'))
+        print ">> Deploying to {} <<".format(plugin_dir)
+        # clean the deployment
+        clean_deployment(False)
         # compile to make sure everything is fresh
+        print 'Compiling to make sure install is clean'
         compile_files()
-        click.echo('Deploying it')
+        #click.echo('Deploying it')
         # get the cfg
-        cfg = config()
-        python_files = cfg.get('files', 'python_files').split()
-        main_dialog = cfg.get('files', 'main_dialog').split()
-        compiled_ui_files = cfg.get('files', 'compiled_ui_files').split()
-        extras = cfg.get('files', 'extras').split()
-        # merge the file lists
-        install_files = python_files + main_dialog + compiled_ui() + compiled_resource() + extras
-        click.echo(install_files)
+        install_files = get_install_files()
+        # make the plugin directory if it doesn't exist
+        if not os.path.exists(plugin_dir):
+            os.mkdir(plugin_dir)
+
+        try:
+            for file in install_files:
+                print "Copying {}".format(file)
+                shutil.copy(file, os.path.join(plugin_dir, file))
+            help_src = config().get('help', 'dir')
+            help_target = os.path.join(
+                get_plugin_directory(),
+                config().get('plugin', 'name'),
+                config().get('help', 'target'))
+            print "Copying {} to {}".format(help_src, help_target)
+            shutil.copytree(help_src, help_target)
+        except OSError as oops:
+            print "Error copying files: {}".format(oops.strerror)
+            print "To ensure proper deployment, delete the deployed plugin before continuing."
+
+
+def clean_deployment(ask_first=True):
+    """ Remove the deployed plugin from the .qgis2/python/plugins directory
+    """
+    name = config().get('plugin', 'name')
+    plugin_dir = os.path.join(get_plugin_directory(), name)
+    if ask_first:
+        proceed = click.confirm('Delete the deployed plugin from {}?'.format(plugin_dir))
+    else:
+        proceed = True
+
+    if proceed:
+        click.echo('Removing from {}'.format(plugin_dir))
+        try:
+            shutil.rmtree(plugin_dir)
+            return True
+        except OSError as oops:
+            print 'Plugin was not deleted: {}'.format(oops.strerror)
+    else:
+        click.echo('Plugin was not deleted')
+    return False
 
 
 @cli.command()
 def dclean():
-    """ Remove the deployed plugin from the .qgis2/python/plugins directory
-    """
-    click.echo('Removing from .qgis2/python/plugins')
+    clean_deployment()
 
 @cli.command()
 def clean():
     """ Remove compiled resource and ui files
     """
+    files = compiled_ui() + compiled_resource()
     click.echo('Cleaning resource and ui files')
+    for file in files:
+        try:
+            os.unlink(file)
+            print "Deleted: {}".format(file)
+        except OSError as oops:
+            print "Couldn't delete {}: {}".format(file, oops.strerror)
+
 
 @cli.command()
 def compile():
@@ -59,13 +125,16 @@ def compile():
 @cli.command()
 def doc():
     """ Build the docs using sphinx"""
-    click.echo('Building the help documentation')
-    if sys.platform == 'win32':
-        makeprg = 'make.bat'
+    if os.path.exists('help'):
+        click.echo('Building the help documentation')
+        if sys.platform == 'win32':
+            makeprg = 'make.bat'
+        else:
+            makeprg = 'make'
+        os.chdir('help')
+        subprocess.check_call([makeprg, 'html'])
     else:
-        makeprg = 'make'
-    os.chdir('help')
-    subprocess.check_call([makeprg, 'html'])
+        print "No help directory exists in the current directory"
 
 @cli.command()
 def zip():
@@ -74,10 +143,22 @@ def zip():
     Pluging repository"""
 
     name = config().get('plugin', 'name', None)
-    if name:
-        zip = zipfile.ZipFile("{}.zip".format(name), "w")
-    else:
-        click.echo("Your config file is missing the plugin name (name=parameter)")
+    confirm = click.confirm(
+        'Create a packaged plugin ({}.zip) from the deployed files?'.format(name))
+    if confirm:
+        # delete the zip if it exists
+        if os.path.exists('{}.zip'.format(name)):
+            os.unlink('{}.zip'.format(name))
+        if name:
+            cwd = os.getcwd()
+            os.chdir(get_plugin_directory())
+            subprocess.check_call(['zip', '-r',
+                                   os.path.join(cwd, '{}.zip'.format(name)),
+                                   name])
+
+            print ('The {}.zip archive has been created in the current directory'.format(name))
+        else:
+            click.echo("Your config file is missing the plugin name (name=parameter)")
 
 
 def validate():
@@ -90,30 +171,43 @@ def config():
     """
     Read the config file pb_tools.cfg and return it
     """
-    cfg = ConfigParser.ConfigParser()
-    cfg.read('pb_tool.cfg')
-    #click.echo(cfg.sections())
-    return cfg
+    if os.path.exists('pb_tool.cfg'):
+        cfg = ConfigParser.ConfigParser()
+        cfg.read('pb_tool.cfg')
+        #click.echo(cfg.sections())
+        return cfg
+    else:
+        print "There is no pb_tool.cfg file in the current directory"
+        print "We can't do anything without it"
+        sys.exit(1)
 
 def compiled_ui():
     cfg = config()
-    uis = cfg.get('files', 'compiled_ui_files').split()
-    compiled = []
-    for ui in uis:
-        (base, ext) = os.path.splitext(ui)
-        compiled.append('{}.py'.format(base))
-    #print "Compiled UI files: {}".format(compiled)
-    return compiled
+    try:
+        uis = cfg.get('files', 'compiled_ui_files').split()
+        compiled = []
+        for ui in uis:
+            (base, ext) = os.path.splitext(ui)
+            compiled.append('{}.py'.format(base))
+        #print "Compiled UI files: {}".format(compiled)
+        return compiled
+    except ConfigParser.NoSectionError as oops:
+        print oops.message
+        sys.exit(1)
 
 def compiled_resource():
     cfg = config()
-    res_files = cfg.get('files', 'resource_files').split()
-    compiled = []
-    for res in res_files:
-        (base, ext) = os.path.splitext(res)
-        compiled.append('{}_rc.py'.format(base))
-    #print "Compiled resource files: {}".format(compiled)
-    return compiled
+    try:
+        res_files = cfg.get('files', 'resource_files').split()
+        compiled = []
+        for res in res_files:
+            (base, ext) = os.path.splitext(res)
+            compiled.append('{}_rc.py'.format(base))
+        #print "Compiled resource files: {}".format(compiled)
+        return compiled
+    except ConfigParser.NoSectionError as oops:
+        print oops.message
+        sys.exit(1)
 
 def compile_files():
     # Compile all ui and resource files
@@ -146,3 +240,33 @@ def compile_files():
     print "Compiled {} resource files".format(res_count)
 
 
+def copy(source, destination):
+    """Copy files recursively.
+
+    Taken from: http://www.pythoncentral.io/
+                how-to-recursively-copy-a-directory-folder-in-python/
+
+    :param source: Source directory.
+    :type source: str
+
+    :param destination: Destination directory.
+    :type destination: str
+
+    """
+    try:
+        shutil.copytree(source, destination)
+    except OSError as e:
+        # If the error was caused because the source wasn't a directory
+        if e.errno == errno.ENOTDIR:
+            shutil.copy(source, destination)
+        else:
+            print('Directory not copied. Error: %s' % e)
+
+def get_plugin_directory():
+    if sys.platform == 'win32':
+        home = os.path.join(os.environ['HOMEDRIVE'], os.environ['HOMEPATH'])
+    else:
+        home = os.environ['HOME']
+    qgis2 = os.path.join('.qgis2', 'python', 'plugins')
+
+    return os.path.join(home, qgis2)
