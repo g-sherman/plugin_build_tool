106f23b240e86fc9275239351707b6ba3c1af1ae
Gary Sherman <gsherman@geoapt.com>
2014-09-30 14:31:08 -0800

    Added needed metadata for pypi

--- pb_tool/setup.py
+++ pb_tool/setup.py
@@ -2,7 +2,20 @@ from setuptools import setup
 
 setup(
     name='pb_tool',
-    version='0.1',
+    version='1.0a1',
+    description='A tool to aid in QGIS Python plugin development',
+    url='https://github.com/g-sherman/plugin_build_tool',
+    author='Gary Sherman',
+    author_email='gsherman@geoapt.com',
+    license='GPL2',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Topic :: Scientific/Engineering :: GIS'],
+
+    keywords='QGIS PyQGIS',
     py_modules=['pb_tool'],
     install_requires=[
         'Click',
